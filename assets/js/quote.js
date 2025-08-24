a36bd4a059071581fa6cf9f0d58c9e6270ef12bc
madarom <graciaandriamihamina@gmail.com>
2025-08-24 15:45:55 +0300

    quote

--- assets/js/quote.js
+++ assets/js/quote.js
@@ -21,7 +21,7 @@ function renderQuotes(data) {
     tbody.innerHTML += `
       <tr class="hover:bg-gray-50">
         <td class="px-6 py-4 text-sm flex items-center gap-2">
-          <div class="w-15 h-15 flex justify-center items-center bg-gray-50 rounded-full">
+          <div class="w-16 h-16 flex justify-center items-center bg-gray-50 rounded-full">
             <i class="fas fa-file-pdf text-gray-400 text-2xl"></i>
           </div>
           ${quote.quote_number || 'N/A'}
